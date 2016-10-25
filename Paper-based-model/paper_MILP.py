97d14bd9b276173e60c8160db59f29b7086cc8df
Temila <temila.ms06f@gmail.com>
2016-10-25 21:06:03 +0200

    implement heuristics

--- Paper-based-model/paper_MILP.py
+++ Paper-based-model/paper_MILP.py
@@ -80,7 +80,69 @@ for t in range(T):
 
 model.setObjective(quicksum((E[t]*C[t] for t in range(T))), GRB.MINIMIZE)
 model.update()
+# model.optimize()
+
+####################################################################################
+
+# Implement Heuristics
+
+intvars = []
+for v in model.getVars():
+    if v.vType != GRB.CONTINUOUS:
+        intvars += [v]
+        v.vType = GRB.CONTINUOUS
+
+model.params.outputFlag = 0
+
 model.optimize()
+
+def sortkey(v1):
+    sol = v1.x
+    return abs(sol-int(sol+0.5))
+
+# Perform multiple iterations.  In each iteration, identify the first
+# quartile of integer variables that are closest to an integer value in the
+# relaxation, fix them to the nearest integer, and repeat.
+
+for iter in range(1000):
+
+# create a list of fractional variables, sorted in order of increasing
+# distance from the relaxation solution to the nearest integer value
+
+    fractional = []
+    for v in intvars:
+        sol = v.x
+        if abs(sol - int(sol+0.5)) > 1e-5:
+            fractional += [v]
+
+    fractional.sort(key=sortkey)
+
+    print('Iteration %d, obj %g, fractional %d' % \
+          (iter, model.objVal, len(fractional)))
+
+    if len(fractional) == 0:
+        print('Found feasible solution - objective %g' % model.objVal)
+        break
+
+
+# Fix the first quartile to the nearest integer value
+    nfix = max(int(len(fractional)/4), 1)
+    for i in range(nfix):
+        v = fractional[i]
+        fixval = int(v.x+0.5)
+        v.lb = fixval
+        v.ub = fixval
+        print('  Fix %s to %g (rel %g)' % (v.varName, fixval, v.x))
+
+    model.optimize()
+
+# Check optimization result
+
+    if model.status != GRB.Status.OPTIMAL:
+        print('Relaxation is infeasible')
+        break
+
+
 for b in range(batch):
     for m in range(machine):
         for t in range(T):
